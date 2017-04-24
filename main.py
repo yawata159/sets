31f0b681fc3ab5fd77c4f357f15a59ce152c6ea6
yawata159 <seijiyawata@gmail.com>
2017-04-24 17:22:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-union = lambda a,b: [x for x in a+b]
+union = lambda a,b: list(set([x for x in a+b]))
 intersection = lambda a,b: [x for x in a if x in b]
 set_diff = lambda u,a: [x for x in u if x not in a]
 symmetric_diff = lambda a,b: set_diff(union(a,b), intersection(a,b))
