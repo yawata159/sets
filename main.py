9ac81e5194c9ff86fa9b343ab8efe808cdd0f27f
yawata159 <seijiyawata@gmail.com>
2017-04-24 14:22:41 -0400

    add fxns

---
+++ main.py
@@ -0,0 +1,5 @@
+union = lambda a,b: [x for x in a+b]
+intersection = lambda a,b: [x for x in a if x in b]
+set_diff = lambda u,a: [x for x in u if x not in a]
+symmetric_diff = lambda a,b: set_diff(union(a,b), intersection(a,b))
+cartesian_prod = lambda a,b: [(x,y) for x in a for y in b]
